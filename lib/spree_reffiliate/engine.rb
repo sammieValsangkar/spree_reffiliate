e2ba194b55c734c2bddba75338a69b586fa730f2
sammyValsangkar <sameer@cart91.com>
2017-09-23 19:15:59 +0530

    expiry span added in config

--- lib/spree_reffiliate/engine.rb
+++ lib/spree_reffiliate/engine.rb
@@ -13,6 +13,10 @@ module SpreeReffiliate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+
+      Spree::AppConfiguration.class_eval do
+        preference :cookie_life_span, :integer, default: 1
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
