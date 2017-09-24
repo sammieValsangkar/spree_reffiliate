a86caac96269c69353e39b0cf66e27222fd51b88
sammyValsangkar <sameer@cart91.com>
2017-09-24 16:44:12 +0530

    migration for referred products

---
+++ db/migrate/20170924105701_create_spree_referred_products.rb
@@ -0,0 +1,9 @@
+class CreateSpreeReferredProducts < ActiveRecord::Migration
+  def change
+    create_table :spree_referred_products do |t|
+      t.integer :product_id, add_index: true
+      t.integer :order_id, add_index: true
+      t.timestamps
+    end
+  end
+end
