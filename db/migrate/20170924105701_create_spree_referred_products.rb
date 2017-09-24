b5fd15f6519eee2792fc2266650ec52b68c41dd6
sammyValsangkar <sameer@cart91.com>
2017-09-24 16:50:46 +0530

    refered_order added instead of order in refered products

--- db/migrate/20170924105701_create_spree_referred_products.rb
+++ db/migrate/20170924105701_create_spree_referred_products.rb
@@ -2,7 +2,7 @@ class CreateSpreeReferredProducts < ActiveRecord::Migration
   def change
     create_table :spree_referred_products do |t|
       t.integer :product_id, add_index: true
-      t.integer :order_id, add_index: true
+      t.integer :referred_order_id, add_index: true
       t.timestamps
     end
   end
