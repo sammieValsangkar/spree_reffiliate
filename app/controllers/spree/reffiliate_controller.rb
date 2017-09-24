da872887a59a0f77dee32bf7b67e40bdf2db99ad
sammyValsangkar <sameer@cart91.com>
2017-09-24 16:57:15 +0530

    product added in affiliate url

--- app/controllers/spree/reffiliate_controller.rb
+++ app/controllers/spree/reffiliate_controller.rb
@@ -9,7 +9,7 @@ module Spree
       cookies[:affiliate] = { :value => params[:path], :expires => Time.now + Spree::Config[:cookie_life_span].to_i.day }
       affiliate = Spree::Affiliate.find_by(:path => params[:path])
       if affiliate.nil? or affiliate.partial.blank? or !partial_exists affiliate.partial
-        redirect_to(root_path)
+        redirect_to_product_if_exist
       elsif partial_exists affiliate.partial
         render "spree/affiliates/#{affiliate.partial}", :layout => affiliate.get_layout
       end
@@ -18,6 +18,23 @@ module Spree
     private
       def partial_exists partial
         Affiliate.lookup_for_partial lookup_context, partial
+      end
+
+      def redirect_to_product_if_exist
+        if params[:p]
+          product = Spree::Product.published.find_by_slug(params[:p])
+          if session[:affiliated_products]
+            session[:affiliated_products] << product.id
+          else
+            session[:affiliated_products] = [ product.id ]
+          end
+        end
+        Rails.logger.info "#{'->>' * 200} #{session[:affiliated_products]}"
+        if product
+          redirect_to product_path(product)
+        else
+          redirect_to(root_path)
+        end
       end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Spree::Core::Engine.routes.draw do
   get 'r/:code' => 'reffiliate#referral', as: 'referral'
   get 'a/:path' => 'reffiliate#affiliate', as: 'affiliate'
+  get 'a/:path/:p' => 'reffiliate#affiliate', as: 'affiliate_product'
 
   namespace :admin do
     resources :affiliates
